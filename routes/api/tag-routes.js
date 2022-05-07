09c3ba49d5d86422d651448a77518f8cae2b8e40
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-05-07 16:07:08 -0700

    created delete tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -86,8 +86,18 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(deletedTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
