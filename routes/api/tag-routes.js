f40cd99faf2e96df1ddc63f829dbb199ee977b9d
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-05-07 16:04:20 -0700

    created get and post routes for Tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,15 +6,55 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: {
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+    },
+  })
+    .then((TagData) => {
+      res.status(200).json(TagData);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    });
+    if (!tagData) {
+      throw {
+        status: 404,
+        message: "No Tag found with this ID",
+      };
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    if (err.status === 404) {
+      res.status(404).json(err);
+    } else {
+      res.status(500).json(err);
+    }
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  // create a new tag    
+    try {
+    const newTag = await Tag.create(req.body)
+    res.status(201).json(newTag)
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
